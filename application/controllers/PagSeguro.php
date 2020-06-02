9c1b8b9442c533fecf55985229d858a660b13ff8
marcoalvesalmeida <marcoalvesneto@gmail.com>
2020-06-02 07:54:45 -0300

    Get Session PagSeguro

---
+++ application/controllers/PagSeguro.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class PagSeguro extends CI_Controller {
+
+	public function session()
+	{
+		$url = "https://ws.sandbox.pagseguro.uol.com.br/v2/sessions?email=seuemail&token=seutoken";
+        $curl = curl_init($url);
+        curl_setopt($curl,CURLOPT_HTTPHEADER,array("Content-Type: application/x-www-form-urlencoded; charset=UTF-8"));
+        curl_setopt($curl,CURLOPT_POST,1);
+        curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,false);
+        curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+        $response=curl_exec($curl);
+        curl_close($curl);
+
+        $xml=simplexml_load_string($response);
+        echo json_encode($xml);
+	}
+}
